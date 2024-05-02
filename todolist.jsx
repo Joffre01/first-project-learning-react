e2bcd2e5b8ecfee3718058a11e358ca70dfe83a2
Obenson Joffre <94082079+Joffre01@users.noreply.github.com>
2024-05-01 23:33:13 -0400

    Add files via upload

---
+++ todolist.jsx
@@ -0,0 +1,15 @@
+
+import { TodoItem } from "./todoItem"
+export function TodoList({todos, toggleTodo, deleteTodo}){
+    return(
+        <ul className='list'>
+            {todos.length === 0 && 'None'}
+            {todos.map(todo => {
+                return ( 
+                 <TodoItem {...todo} key={todo.id} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
+    
+      )
+    })}
+  </ul>
+    )
+}
